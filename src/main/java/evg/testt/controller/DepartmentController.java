5851f6bced3a3b335cd94ce1409488845c44940a
Denis <1vds@mail.ru>
2016-11-05 02:01:01 +0300

    Update DepartmentController.java

--- src/main/java/evg/testt/controller/DepartmentController.java
+++ src/main/java/evg/testt/controller/DepartmentController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.sql.SQLException;
 
 @Controller
-public class DepartmentController {
+public class DepartmentController2 {
 
     @Autowired
     DepartmentService departmentService;
